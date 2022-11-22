9f57dbed5ebc64b60b63861382c5f9d6d0d8b2e0
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-11-22 12:49:35 +0200

    Delete swagger comments, db dumb added

--- routes/api/authorsRoute.js
+++ routes/api/authorsRoute.js
@@ -1,21 +1,3 @@
-// /**
-// * @swagger
-// * components:
-// *   schemas:
-// *       Author:
-// *           type: object
-// *           required:
-// *               — name
-// *           properties:
-// *               id:
-// *                   type: integer
-// *                   description: The auto-generated id of the author.
-// *               full_name:
-// *                   type: string
-// *                   description: The full name of author.
-// *           example:
-// *               name: RESTful API
-// */
 module.exports = app => {
     const authors = require('../../controllers/authorController');
     const router = require('express').Router();
@@ -29,33 +11,5 @@ module.exports = app => {
     // Update author
     router.put('/', authors.update);
     
-    // /**
-    // * @swagger
-    // * /api/authors:
-    // *    get:
-    // *       summary: Retrieve a list of authors.
-    // *       description: Retrieve a List of authors.
-    // *       responses:
-    // *           200:
-    // *               description: A List of authors:
-    // *               content:
-    // *                   application/json:
-    // *                       schema:
-    // *                           type: object
-    // *                           properties:
-    // *                               data:
-    // *                                   type: array
-    // *                                   items:
-    // *                                       type: object
-    // *                                       properties:
-    // *                                           id:
-    // *                                               type: integer
-    // *                                               description: The author ID.
-    // *                                               example: 1
-    // *                                           full_name:
-    // *                                               type: string
-    // *                                               description: The author's full name.
-    // */
-   
    app.use('/api/authors', router)
 }
--- routes/api/categoriesRoute.js
+++ routes/api/categoriesRoute.js
@@ -1,21 +1,3 @@
-// /**
-// * @swagger
-// * components:
-// *   schemas:
-// *       Category:
-// *           type: object
-// *           required:
-// *               — name
-// *           properties:
-// *               id:
-// *                   type: integer
-// *                   description: The auto-generated id of the category.
-// *               name:
-// *                   type: string
-// *                   description: The name of category.
-// *           example:
-// *               name: Open Source
-// */
 module.exports = app => {
     const categories = require('../../controllers/categoryController');
     const router = require('express').Router();
@@ -32,27 +14,6 @@ module.exports = app => {
 
     // Get categories and books amount in each category
     router.get('/b-count', categories.getBooksCountInCategory)
-
-    // /**
-    // * @swagger
-    // * /api/categories:
-    // *    get:
-    // *       summary: Retrieve a list of categories.
-    // *       responses:
-    // *           200:
-    // *               description: A List of books:
-    // *               content:
-    // *                   application/json:
-    // *                       schema:
-    // *                           type: object
-    // *                           properties:
-    // *                               id:
-    // *                                   type: integer
-    // *                                   description: The auto-generated id of the category
-    // *                               name: 
-    // *                                   type: string
-    // *                                   description: The name of category
-    // */
 
     app.use('/api/categories', router)
 }
